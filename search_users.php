fbe703a6bed20b4af623e9e3b35459cbd545b58b
Unknown <up201605330@g.uporto.pt>
2018-11-07 22:17:14 +0000

    Some more things and trying to merge

--- search_users.php
+++ search_users.php
@@ -1,5 +1,20 @@
 <?php
 
-echo "Search users";
+$dbh = new PDO('sqlite:database.db');
+
+$name = $_POST['userName'];
+
+$stmt = $dbh->prepare('SELECT personID FROM Person WHERE personName=?');
+$stmt->execute(array($name));
+
+$result = $stmt->fetchAll();
+
+if($result != null){
+	foreach ($result as $row) {
+  		echo $row['personID'];
+  	}
+} else{
+	echo "We could not find users with that name!";
+}
 
 ?>
