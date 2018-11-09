2c3bd79f794ed6303e5c8980a29629c58e03a5ad
Unknown <up201605330@g.uporto.pt>
2018-11-09 14:14:12 +0000

    Nothing

--- login.php
+++ login.php
@@ -6,7 +6,6 @@ include_once("../session.php");
 
     if($userID = checkPassword($_POST['username'], $_POST['passW']) != -1) 
         currentUser($_POST['username'], $userID);
-    
     else
         $_SESSION['ERROR'] = 'Incorrect password or username';
 
