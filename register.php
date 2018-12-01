f4f4bc4474e23d5d05b167e65f6ce9a70dab76e7
João Ferreira <up201605330@g.uporto.pt>
2018-12-01 17:36:41 +0000

    Register and the database check already working with the new database

--- register.php
+++ register.php
@@ -2,7 +2,6 @@
 
 $dbh = new PDO('sqlite:database.db');
 
-$id = 39;
 $name = $_POST['name'];
 $email = $_POST['email'];
 $username = $_POST['username'];
@@ -10,11 +9,15 @@ $password = $_POST['password'];
 $repassword = $_POST['repassword'];
 $age = $_POST['age'];
 
-$stmt = $dbh->prepare('INSERT INTO Person (personID, personName, personAge) VALUES (?, ?, ?);');
-$stmt->execute(array($id, $name, $age));
+echo $name, "<br><br>";
+echo $email, "<br><br>";
+echo $username, "<br><br>";
+echo $password, "<br><br>";
+echo $repassword, "<br><br>";
+echo $age, "<br><br>";
 
-$stmt = $dbh->prepare('INSERT INTO Account (accountID, personID, passW, email, username) VALUES (?, ?, ?, ?, ?);');
-$stmt->execute(array($id, $id, $password, $email, $username));
+$stmt = $dbh->prepare('INSERT INTO Account (accountID, personName, passW, email, username, birthday, city, job, photo) VALUES (NULL, ?, ?, ?, ?, ?, NULL, NULL, \'default.png\');');
+$stmt->execute(array($name, $password, $email, $username, $age));
 
 echo "User registered with success";
 ?>
