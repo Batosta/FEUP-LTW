5a7aeac699d8ae4c8533e64d95f48b70b4be99e7
João Ferreira <up201605330@g.uporto.pt>
2018-12-01 19:00:20 +0000

    Refactored wall

--- user_functions.php
+++ user_functions.php
@@ -79,5 +79,58 @@
 
         return $row['accountID'];
     }
-
 ?>
+
+<?  function showPostByPostId($dbh, $postID){
+
+        $stmt = $dbh->prepare('SELECT * FROM Post WHERE postID = ?');
+        $stmt->execute(array($postID));
+        $post = $stmt->fetch();
+
+        $post_id = $post['postID'];
+        $account_id = $post['accountID']; ?>
+
+        <div class="post">
+            <section id="info">
+                <?
+                    $post_photo = getAccountPhoto($dbh, $account_id);
+                    $post_username = getAccountUsername($dbh, $account_id);
+                ?>
+                <img id="account_photo" src=<?=$post_photo?> alt="Account photo" height="50" width="50">
+                <h2 id="username"><?=$post_username?></h2>
+                <img id="post_photo" src=<?=$post['photo']?> alt="Post photo">
+                <h3 id="description"><?=$post['description']?></h3>
+            </section>
+            <section id="options">
+                <h4>like, comment, share</h4>
+            </section>
+            <?
+                $stmt1 = $dbh->prepare('SELECT DISTINCT accountID, commentText FROM Comment WHERE postID = ?');
+                $stmt1->execute(array($post_id));
+                $comments = $stmt1->fetchAll();
+                foreach ($comments as $comment) {
+            ?>
+            <section id="comments">
+                <?
+                    $comment_photo = getAccountPhoto($dbh, $comment['accountID']);
+                ?>
+                <img id="comment_photo" src=<?=$comment_photo?> alt="Comment photo" height="40" width="40">
+                <h4><?=$comment['commentText']?></h4>
+                <?
+                    $stmt4 = $dbh->prepare('SELECT DISTINCT accountID, sucommentText FROM SubComment WHERE commentID = ?');
+                    $stmt4->execute(array($comment[0]));
+                    $subcomments = $stmt4->fetchAll();
+                    foreach ($subcomments as $subcomment) {
+                ?>
+                <section id="subcomments">
+                    <?
+                        $subcomment_photo = getAccountPhoto($dbh, $subcomment['accountID']);
+                    ?>
+                    <img id="subcomment_photo" src=<?=$subcomment_photo?> alt="Subcomment photo" height="30" width="30">
+                    <h5><?=$subcomment['sucommentText']?></h5>
+                </section>
+                <? } ?>
+            </section>
+            <? } ?>
+        </div>
+<? } ?>
