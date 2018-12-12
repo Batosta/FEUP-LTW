faa09ee19623908ac0961b1c253ef4c35e5932c0
João Ferreira <up201605330@g.uporto.pt>
2018-12-12 17:20:00 +0000

    Refactored a bit

--- change_profile.php
+++ change_profile.php
@@ -2,6 +2,7 @@
 <html lang="en-US"> 
   <?
     include ('./session.php');
+    include ('./user_functions.php');
   ?>
   <head>
     <title>Edit Profile</title> 
@@ -9,32 +10,8 @@
     <link href="profile.css" rel="stylesheet">
   </head>
   <body>
-    <div class="navBar">
-
-      <div class="menu">
-          <img src="imagens/menu.png" height="25" width="25">
-          <div class="menuContent">
-            <a id="homePage" href="profile.php"><p>Profile</p></a>
-            <a id="settings" href="change_profile.php"><p>Edit profile</p></a>
-            <a id="subs" href="subscriptions.php"><p>Subscriptions</p></a>
-            <a id="likes" href="#"><p>Liked</p></a>
-          </div>
-      </div>
-
-      <div class="searchBar">
-          <form>
-              <input type="text" name="search" placeholder="Search something in your profile">
-          </form>
-      </div>
-      
-      <div class="logOut">
-          <img src="imagens/turn-on.png" height="25" width="25">
-          <div class="logOutContent">
-            <a id="logOut" href="logout.php"><p>Sign Out</p></a>
-            <a id="user" href="logotheraccount.php"><p>Sign into other account </p></a>
-          </div>
-      </div>
-    </div>
+    
+    <? draw_header(); ?>
 
     <div class="main">
       <form id="change" action="change_profile_functions.php" method="post">
@@ -64,16 +41,6 @@
     </div>
   </body>
 
-  <footer>
-    <div class="about">
-      <p>About Us  <p>
-    </div>
-    <div class="ltw">
-      <a id="ltw" href="https://web.fe.up.pt/~arestivo/page/courses/2018/ltw/" target="_blank"><p>Linguagens & Tecnoglogias Web</p></a>
-    </div>
-    <div class="contacts">
-      <p> Contact </p>
-    </div>
-  </footer>
+  <? draw_footer(); ?>
 
 </html>
