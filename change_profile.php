280e5069c2f01a4b27d5f918f73dc4c197b172e2
João Ferreira <up201605330@g.uporto.pt>
2018-12-12 16:08:40 +0000

    And another one

--- change_profile.php
+++ change_profile.php
@@ -4,18 +4,38 @@
     include ('./session.php');
   ?>
   <head>
-    <title>Profile</title> 
+    <title>Edit Profile</title> 
     <link href="favicon.ico" rel="shortcut icon">
-    <link href="profile_style.css" rel="stylesheet">
-    <link href="profile_layout.css" rel="stylesheet">
+    <link href="profile.css" rel="stylesheet">
   </head>
   <body>
-    <div class="header">
-      <h1><a href="wall.php">ONLINE 420</a></h1>
-      <form id="logout" action="logout.php">
-        <input type="submit" value="Log out" >
-      </form>
+    <div class="navBar">
+
+      <div class="menu">
+          <img src="imagens/menu.png" height="25" width="25">
+          <div class="menuContent">
+            <a id="homePage" href="profile.php"><p>Profile</p></a>
+            <a id="settings" href="change_profile.php"><p>Edit profile</p></a>
+            <a id="subs" href="subscriptions.php"><p>Subscriptions</p></a>
+            <a id="likes" href="#"><p>Liked</p></a>
+          </div>
+      </div>
+
+      <div class="searchBar">
+          <form>
+              <input type="text" name="search" placeholder="Search something in your profile">
+          </form>
+      </div>
+      
+      <div class="logOut">
+          <img src="imagens/turn-on.png" height="25" width="25">
+          <div class="logOutContent">
+            <a id="logOut" href="logout.php"><p>Sign Out</p></a>
+            <a id="user" href="logotheraccount.php"><p>Sign into other account </p></a>
+          </div>
+      </div>
     </div>
+
     <div class="main">
       <form id="change" action="change_profile_functions.php" method="post">
         <input type="text" name="new_info" placeholder="Change Name">
@@ -43,4 +63,17 @@
       </form>
     </div>
   </body>
+
+  <footer>
+    <div class="about">
+      <p>About Us  <p>
+    </div>
+    <div class="ltw">
+      <a id="ltw" href="https://web.fe.up.pt/~arestivo/page/courses/2018/ltw/" target="_blank"><p>Linguagens & Tecnoglogias Web</p></a>
+    </div>
+    <div class="contacts">
+      <p> Contact </p>
+    </div>
+  </footer>
+
 </html>
