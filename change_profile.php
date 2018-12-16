afd9805180bf7966aae76c1551d1b5474d54e7af
Gwenevere <up201604741@fe.up.pt>
2018-12-16 15:21:57 +0000

    working on edit profile

--- change_profile.php
+++ change_profile.php
@@ -12,15 +12,31 @@
   ?>
   <head>
     <title>Edit Profile</title> 
-    <link href="favicon.ico" rel="shortcut icon">
-    <link href="profile.css" rel="stylesheet">
     <link href="common.css" rel="stylesheet">
+    <link href="change_profile.css" rel="stylesheet">
+    <link href="imagens/icon.png" rel="shortcut icon">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300, 300i|Quicksand:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300" rel="stylesheet">
   </head>
   <body>
     
     <? draw_header($accountPhoto, $accountUsername); ?>
 
     <div class="main">
+
+      <div class="profilePic">
+        <h2> Edit Profile </h2>
+        <img src=<?=$accountPhoto ?> alt="Profile photo" height="150" width="150">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload: 
+        <p id="fakeButton">Choose image</p>
+        <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
+        <input id="submit" type="submit" value="Upload Image" name="submit">
+      </form>
+    </div>
+
+      <div class="editInfo"> 
       <form id="change" action="change_profile_functions.php" method="post">
         <input type="text" name="new_info" placeholder="Change Name">
         <input type="submit" class="button" name="change_name" value="Change" >
@@ -46,6 +62,8 @@
         <input type="submit" class="button" name="change_job" value="Change" >
       </form>
     </div>
+  </div>
+
   </body>
 
   <? draw_footer(); ?>
